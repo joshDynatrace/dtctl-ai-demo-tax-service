f3234bae2abea20dc63076b453f537264a02e9c1
joshDynatrace <joshua.hendrick@dynatrace.com>
2026-04-07 16:59:09 -0700

    Added example

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 set -euo pipefail
 
 docker buildx build --platform linux/amd64,linux/arm64 -t tax-service:latest .
+
+# example to build and push:
+# docker buildx build --platform linux/amd64,linux/arm64 -t jhendrick/tax-service:latest . --push
